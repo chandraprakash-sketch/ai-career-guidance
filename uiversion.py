e7cbf11396afead3aace7ae1e42c3eef5a4a9406
chandraprakash <chandraprakash.peerlearnz@gmail.com>
2026-02-24 22:14:34 +0530

    remove railway.json

--- uiversion.py
+++ uiversion.py
@@ -140,7 +140,5 @@ def result():
                            goal=goal,
                            data=english)
 
-import os
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
